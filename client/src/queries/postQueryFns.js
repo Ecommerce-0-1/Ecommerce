786276d07ee734d1d3f7da23ca5f9b338fd5eb14
Mohammed <mohammedhassouna000@gmail.com>
2024-12-06 22:02:03 +0300

    Add client folders

---
+++ client/src/queries/postQueryFns.js
@@ -0,0 +1,9 @@
+import api from "../configs/api";
+
+// Sign Up
+// export async function signUpUser(formData) {
+//   const { data } = await api.post(`/user/register`, { ...formData });
+//   return data;
+// }
+
+
