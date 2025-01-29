7919cad2fb415d2083bdf167098079b128cb7373
mohammed <mohammedhassouna000@gmail.com>
2025-01-29 20:05:24 +0300

    Add Products CRUD

--- server/app/Models/Products.php
+++ server/app/Models/Products.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,12 +16,81 @@ class Products extends Model
     protected $table = 'products';
     protected $fillable = [
         'name',
-        'email',
+        'description',
+        'price',
+        'category_id',
+        'qty',
+        'img',
+        'rating',
     ];
 
     public function category(): BelongsTo
     {
         return $this->belongsTo(Categories::class);
+    }
+
+    protected static function CreateProduct($product)
+    {
+        try {
+            return self::create($product);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public static function CreateMultipleProducts(array $products)
+    {
+        try {
+            foreach ($products as &$product) {
+                $product['created_at'] = now();
+                $product['updated_at'] = now();
+            }
+            self::insert($products);
+            
+            $names = array_column($products, 'name');
+        
+            return self::whereIn('name', $names)->get();
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    protected static function GetProducts()
+    {
+        try {
+            return self::with('category')->get();
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    protected static function GetProductById($id)
+    {
+        try {
+            return self::findorfail($id);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    protected static function UpdateProduct($id, $data)
+    {
+        try {
+            $product = self::findorfail($id);
+            $product->update($data);
+            return $product;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    protected static function DeleteProduct($id)
+    {
+        try {
+            return self::findorfail($id)->delete();
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
 
     public function discount(): HasOne
