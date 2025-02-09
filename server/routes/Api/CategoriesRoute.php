1ebaeccabcdd46afbd6c6fb02b4416257e52dcdf
mohammed <mohammedhassouna000@gmail.com>
2025-02-09 20:01:54 +0300

    Order of naming routes

--- server/routes/Api/CategoriesRoute.php
+++ server/routes/Api/CategoriesRoute.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('get_categories', [CategoriesController::class, 'index'])->name('index');
-Route::get('get_category/{id}', [CategoriesController::class, 'show'])->name('show');
+Route::get('/ctgy/get', [CategoriesController::class, 'index'])->name('index');
+Route::get('/ctgy/get/{id}', [CategoriesController::class, 'show'])->name('show');
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('create_category', [CategoriesController::class, 'store']);
-    Route::post('update_category/{id}', [CategoriesController::class, 'update']);
-    Route::delete('delete_category/{id}', [CategoriesController::class, 'destroy']);
+    Route::post('/ctgy/create', [CategoriesController::class, 'store']);
+    Route::post('/ctgy/update/{id}', [CategoriesController::class, 'update']);
+    Route::delete('/ctgy/delete/{id}', [CategoriesController::class, 'destroy']);
 });
--- server/routes/Api/UsersRoute.php
+++ server/routes/Api/UsersRoute.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/register', [UserController::class, 'register'])->name('register');
-Route::post('/login', [UserController::class, 'login'])->name('login');
-Route::post('/googlelogin', [UserController::class, 'GoogleLogin'])->name('GoogleLogin');
+Route::post('/user/register', [UserController::class, 'register'])->name('register');
+Route::post('/user/login', [UserController::class, 'login'])->name('login');
+Route::post('/user/googlelogin', [UserController::class, 'GoogleLogin'])->name('GoogleLogin');
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/getuser', [UserController::class, 'show']);
-    Route::post('/update_user', [UserController::class, 'update']);
-    Route::delete('/delete_user', [UserController::class, 'destroy']);
+    Route::get('/user/get', [UserController::class, 'show']);
+    Route::post('/user/update', [UserController::class, 'update']);
+    Route::delete('/user/delete', [UserController::class, 'destroy']);
 });
