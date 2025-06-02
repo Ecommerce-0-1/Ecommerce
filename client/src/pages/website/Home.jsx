26fc49af42289cf061b54f72bb2a35b975a5d745
Mohammed <mohammedhassouna000@gmail.com>
2025-06-02 19:44:52 +0300

    Add HomePage & update browserlist data

---
+++ client/src/pages/website/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-6">
+        Welcome to Our E-commerce Store
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
