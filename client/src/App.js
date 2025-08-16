1ab016d4b63ac853b384cbb7195af68953b46bdb
Mohammed <mohammedhassouna000@gmail.com>
2025-08-16 19:09:30 +0300

    Fix App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { reactQueryClientOptions } from "./configs/reactQueryClientOptions";
-import ReactPublicContextProvider from "./providers/PublicContextProvider"
-import ToastifyProvider from "./providers/ToastifyProvider"
-import LoadingProvider from "./providers/LoadingProvider"
+import ReactPublicContextProvider from "./providers/PublicContextProvider";
+import ToastifyProvider from "./providers/ToastifyProvider";
+import LoadingProvider from "./providers/LoadingProvider";
+import { CartProvider } from "./providers/CartProvider";
 import Index from "./pages/index";
 const queryClient = new QueryClient(reactQueryClientOptions);
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools initialIsOpen={false} />
-    <ReactPublicContextProvider>
-      <ToastifyProvider>
-        <LoadingProvider>
-          <Index />
-        </LoadingProvider>
-      </ToastifyProvider>
-    </ReactPublicContextProvider>
-  </QueryClientProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactPublicContextProvider>
+        <ToastifyProvider>
+          <LoadingProvider>
+            <CartProvider>
+              <Index />
+            </CartProvider>
+          </LoadingProvider>
+        </ToastifyProvider>
+      </ReactPublicContextProvider>
+    </QueryClientProvider>
   );
 }
 
